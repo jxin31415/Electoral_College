4cf3b52011cd999eaee059d9c09728e37263ac1a
m1lesmackenzie <44821031+m1lesmackenzie@users.noreply.github.com>
2020-06-21 12:38:22 -0500

    Update Article.dart

--- lib/Article.dart
+++ lib/Article.dart
@@ -1,30 +1,145 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Article extends StatelessWidget {
 
+  Future<void>_launchURL(String url) async {
+    if (await canLaunch(url)){
+      await launch(url);
+    }else{
+      throw "Could not launch $url";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        body: generateArticle(),
-      );
-  } 
+    return MaterialApp(
+      title: "Electoral College",
+      home: Scaffold(
+        appBar: AppBar(
+            leading: IconButton(icon:Icon(Icons.arrow_back),
+              onPressed:() => Navigator.pop(context, false),
+            )
+        ),
+          body: generateArticle()
+      ),
+    );
+  }
 
   Widget generateArticle(){
     return ListView(
       children: <Widget>[
-        SizedBox(height: 20),
-        Container(
-          padding: EdgeInsets.all(10),
-          alignment: Alignment.center,
-          child: Text("What is the Electoral College?",
-            textScaleFactor: 3,
-            textAlign: TextAlign.center,
+        Card(
+          child: Column(
+            children: <Widget>[
+              Text(
+                  "How to Register to Vote",
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ]
           ),
+
         ),
-        Container(
-          padding: EdgeInsets.all(20),
-          child: Text("According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground."),
+        new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+            ExpansionTile(
+              title: Text("Register Online"),
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child:Text("Online voter registration is a more modern way of registering voters and replaces the more traditional paper forms. As of June 2020, 39 states plus the District of Columbia allow online voter registration. Applicants must submit a form of valid identification like a driver's license or state-issued identification card. Online voter registration only implements advanced security measures to ensure that applicants' data is not stolen."),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            ExpansionTile(
+              title: Text("Register by Mail"),
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child:Text("To register to vote by mail, you must fill out the National Mail Voter Registration Form. Each state has their own specific instructions for registering this way. Once the form has been filled out, it can be mailed to a state or local election office."),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            ExpansionTile(
+              title: Text("Register in Person"),
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child:Text("Voters can also be registered in person at a state or local election office. In certain cases, they can also register at the department of motor vehicles, armend forces recruitment centers, and state and county public assistance offices. Each area and facility has their own requirements so be sure to check with your local office first."),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            ExpansionTile(
+              title: Text("Register Overseas"),
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child:Text("If you are a U.S. citizen living outside the U.S., a service member stationed overseas, or a spouse or family member of a service member stationed overseas you can register to vote and request a ballot form."),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            ],
         ),
+        new Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text("Go to usa.gov to learn more about voter registration.",
+                style: TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ),
+            RaisedButton(
+              child: Text("usa.gov"),
+              color: Colors.lightBlue,
+              splashColor: Colors.lightBlueAccent,
+              onPressed: (){
+                _launchURL("https://www.usa.gov/register-to-vote");
+              }
+            ),
+            Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Text("Go to vote.gov to learn about your state's specific voting requirements.",
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                )
+            ),
+            RaisedButton(
+                child: Text("vote.gov"),
+                color: Colors.red,
+                splashColor: Colors.redAccent,
+                onPressed: (){
+                  _launchURL("https://vote.gov/");
+                }
+            ),
+          ],
+        )
+
 
         //TO-DO
       ],
