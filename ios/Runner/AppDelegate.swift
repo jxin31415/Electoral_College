576b4bb9a86473a02f9e85934b4cc2559d86a968
jxin31415 <jxin31415@gmail.com>
2020-08-16 00:11:42 -0500

    Add Notifications for Major Events

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,6 +10,9 @@ import GoogleMaps
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     GMSServices.provideAPIKey("KEY_HERE")
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'Map.dart' as map;
 import 'Candidates.dart' as candidates;
 import 'Home.dart' as home;
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,11 +22,92 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   TabController _tabController;
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
   @override
   void initState() {
     super.initState();
     _tabController = new TabController(vsync: this, length: 5);
+    var initializationSettingsAndroid = new AndroidInitializationSettings("@mipmap/ic_launcher");
+    var initializationSettingsIOS = new IOSInitializationSettings();
+
+    var initializationSettings = new InitializationSettings(initializationSettingsAndroid, initializationSettingsIOS);
+
+    flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+    flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: onSelectNotification);
+
+    scheduleNotifs();
+  }
+
+  Future onSelectNotification(String payload) async {
+    if(payload != null){
+      debugPrint('notification payload: ' + payload);
+    }
+  }
+
+  static const eventlist = [
+    {
+      'id': 'PresidentialDebate1',
+      'channel': 'PresidentialDebate1',
+      'channelDescription': 'Date of the 1st Presidential Debate',
+      'title': 'The 1st Presidential Debate is today!',
+      'description': 'Tune in to watch the candidates debate at the University of Notre Dame in Indiana!',
+    },
+     {
+      'id': 'PresidentialDebate2',
+      'channel': 'PresidentialDebate2',
+      'channelDescription': 'Date of the 2nd Presidential Debate',
+      'title': 'The 2nd Presidential Debate is today!',
+      'description': 'Tune in to watch the candidates debate at the University of Michigan in Ann Arbor!',
+    },
+     {
+      'id': 'PresidentialDebate3',
+      'channel': 'PresidentialDebate3',
+      'channelDescription': 'Date of the 3rd Presidential Debate',
+      'title': 'The 3rd Presidential Debate is today!',
+      'description': 'Tune in to watch the candidates debate at Belmont University in Nashville!',
+    },
+    {
+      'id': 'VPDebate',
+      'channel': 'VPDebate',
+      'channelDescription': 'Date of the Vice Presidential Debate',
+      'title': 'The Vice Presidential Debate is today!',
+      'description': 'Tune in to watch the vice presidential candidates debate at the University of Utah in Salt Lake City!',
+    },
+    {
+      'id': 'Election',
+      'channel': 'Election',
+      'channelDescription': 'Election Day',
+      'title': 'It\'s Election Day!',
+      'description': 'Get out there and vote!',
+    },
+  ];
+  static final dates = [
+    DateTime(2020, 9, 29, 9),
+    DateTime(2020, 10, 15, 9),
+    DateTime(2020, 10, 22, 9),
+    DateTime(2020, 10, 7, 9),
+    DateTime(2020, 11, 3, 8),
+  ];
+
+  Future scheduleNotifs() async {
+    for(var i = 0; i < eventlist.length; i++){
+      var scheduledNotificationDateTime =
+          dates[i];
+      var androidPlatformChannelSpecifics =
+          AndroidNotificationDetails(eventlist[i]['id'],
+              eventlist[i]['channel'], eventlist[i]['channelDescription']);
+      var iOSPlatformChannelSpecifics =
+          IOSNotificationDetails();
+      NotificationDetails platformChannelSpecifics = NotificationDetails(
+          androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+      await flutterLocalNotificationsPlugin.schedule(
+          0,
+          eventlist[i]['title'],
+          eventlist[i]['description'],
+          scheduledNotificationDateTime,
+          platformChannelSpecifics);
+    }
   }
 
   @override
